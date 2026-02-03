421fc67afcf012f29f112d643ff6355a4cecc7d2
chrispok18 <chrispok18@gmail.com>
2026-02-03 21:20:51 +0000

    Create index.js
    feat: add frontend react entry

---
+++ frontend/src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+function App() {
+  return <h1>StartTech Frontend Deployed ✅</h1>;
+}
+
+createRoot(document.getElementById("root")).render(<App />);
